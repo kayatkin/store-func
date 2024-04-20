45c5e327357ab1527ec9a8513948c23639bad49e
Viktor Kayatkin <kayatkin@mail.ru>
2024-04-20 21:57:33 +0700

    add props

--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,24 +1,17 @@
-export const ShopItemFunc = () => {
+export const ShopItemFunc = (props) => {
 
-    const item = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£'
-    };
+    const { brand, title, description, descriptionFull, price, currency } = props.info;
 
     return (
         <div className="main-content">
-            <h2>{item.brand}</h2>
-            <h1>{item.title}</h1>
-            <h3>{item.description}</h3>
-            <div className="description">{item.descriptionFull}</div>
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div className="description">{descriptionFull}</div>
             <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
             <div className="divider"></div>
             <div className="purchase-info">
-                <div className="price">{`${item.currency} ${item.price.toFixed(2)}`}</div>
+                <div className="price">{`${currency} ${price.toFixed(2)}`}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
