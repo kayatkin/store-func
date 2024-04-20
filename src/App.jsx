ffe4a7516e452633d2e1c2934d3fbed392a9e2f5
Viktor Kayatkin <kayatkin@mail.ru>
2024-04-20 21:22:09 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './components/App.css'
+import { ShopItemFunc } from './components/ShopItemFunc'
+
+function App() {
+  
+
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc />
+      </div>
+    </div>
+  )
+
+}
+
+export default App
